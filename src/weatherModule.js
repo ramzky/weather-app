1ff17e715b045f4ada0545897154ffa40b11896b
Joram <joramrebolledo@gmail.com>
2023-09-04 16:55:44 +0800

    Fix http to https

--- src/weatherModule.js
+++ src/weatherModule.js
@@ -1,6 +1,6 @@
 async function weatherData(loc = 'marikina') {
   try {
-    return fetch(`http://api.weatherapi.com/v1/forecast.json?key=9ea9fe41085d4e4f85b115923230209&q=${loc}&days=1&aqi=no&alerts=no`, { mode: "cors" })
+    return fetch(`https://api.weatherapi.com/v1/forecast.json?key=9ea9fe41085d4e4f85b115923230209&q=${loc}&days=1&aqi=no&alerts=no`, { mode: "cors" })
       .then((response) => {
         return response.json();
       })
